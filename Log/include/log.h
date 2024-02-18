966e169189e45a0ff0081f66dc3e7c2bc85778a6
om-3004 <omthakkar3004@gmail.com>
2024-02-18 14:35:03 +0530

    Generalized the Log class with the help of template

--- Log/include/log.h
+++ Log/include/log.h
@@ -9,6 +9,7 @@
 namespace LogSystem {
 	using Util::Date;
 	using Util::String;
+
 	class Log
 	{
 	public:
@@ -21,24 +22,75 @@ namespace LogSystem {
 	public:
 		// default constructor
 		/*explicit inline Log() : m_LogLevel{ Level::LevelWarning } {
-	
+
 		}*/
 
 		// parameterized constructor
-		inline Log(const Level& level = Level::LevelWarning) : m_LogLevel{ level }, m_Date{31, 12, 2022} {
-			
+		inline Log(const Level& level = Level::LevelWarning) : m_LogLevel{ level }, m_Date{ 31, 12, 2022 } {
+
 		}
 
 		void SetLogLevel(const Level& level) {
 			m_LogLevel = level;
 		}
 
-		void setDate(int , int , int );
+		void setDate(int, int, int);
 	
-		void Info(const String&) ;
-		void Warn(const String&);
-		void Error(const String&);
+		template<typename ...Args>
+		void Error(const String& msg, Args ...args);
+		template<typename ...Args>
+		void Warn(const String& msg, Args ...args);
+		template<typename ...Args>
+		void Info(const String& msg, Args ...args);
+
+		void print() {
+			std::cout << '\n';
+		}
+
+		template<typename T, typename ...Args>
+		void print(T arg, Args ...args) {
+			std::cout << arg << " ";
+			print(args...);
+		}
 	};
+	
+	void Log::setDate(int d, int m, int y) {
+		m_Date.setDate(d, m, y);
+	}
+
+	template<typename ...Args>
+	void Log::Error(const String& msg, Args ...args) {
+		if (m_LogLevel >= Log::Level::LevelError) {
+				std::cout << "Date: " << m_Date.getStrDate() << " [Error]: " << msg << " ";
+			if (sizeof...(args) > 0)
+				print(args...);
+			else
+				std::cout << "\n";
+		}
+	}
+
+	template<typename ...Args>
+	void Log::Warn(const String& msg, Args ...args) {
+		if (m_LogLevel >= Log::Level::LevelWarning) {
+				std::cout << "Date: " << m_Date.getStrDate() << " [Warning]: " << msg << " ";
+			if (sizeof...(args) > 0)
+				print(args...);
+			else
+				std::cout << "\n";
+		}
+	}
+	
+	template<typename ...Args>
+	void Log::Info(const String& msg, Args ...args) {
+		if (m_LogLevel >= Log::Level::LevelInfo) {
+			std::cout << "Date: " << m_Date.getStrDate() << " [Info]: " << msg << " ";
+			if (sizeof...(args) > 0)
+				print(args...);
+			else
+				std::cout << "\n";
+		}
+	}
+	
 }
 
 #endif
--- Log/main.cpp
+++ Log/main.cpp
@@ -12,21 +12,23 @@ int main()
 	using namespace Exception;
 
 	//Log log = Log(Log::Level::LevelError);
-	/*Log log = Log();
-	log.SetLogLevel(Log::Level::LevelError);
+	Log log = Log();
+	log.SetLogLevel(Log::Level::LevelWarning);
 
-	log.Info("Fine!");
-	log.Warn("Carefull!");
-	log.Error("Stop!");
+	log.Info("Fine!", 2168);
+	log.Warn("Carefull!", 47685, 'a', "abd");
+	log.Error("Stop!", 574);
+	log.Error("Chup thai jaa", 3.5);
+	log.Error("Chup thai jaa");
 
-	log.Info("Fine!");
-	log.Warn("Carefull!");
-	log.Error("Stop!");
-	log.setDate(23, 4, 2024);
+	//log.Info("Fine!");
+	//log.Warn("Carefull!");
+	//log.Error("Stop!");
+	//log.setDate(23, 4, 2024);
 
-	log.Info("Fine!");
-	log.Warn("Carefull!");
-	log.Error("Stop!");*/
+	//log.Info("Fine!");
+	//log.Warn("Carefull!");
+	//log.Error("Stop!");
 
 	/*String s{ "Om" };
 	try {
@@ -36,44 +38,16 @@ int main()
 		std::cout << e.getMessage() << '\n';
 	}*/
 
-	Date d1{ 31, 12, 2024 };
-	String s{ "Om" };
-	try {
-		d1.setDate( 32, 12, 2024 );
-		auto ch{ s[4] };
-	}
-	catch (InvalidDate& e) {
-		std::cout << e.getMessage() << '\n';
-	}
-	catch (IndexOutOfBound& e) {
-		std::cout << e.getMessage() << '\n';
-	}
-
-	
-
-
-
-	String s1{ "OMTHAKKAR" };
-	s1.to_lower();
-	std::cout << "Lower: " << s1 << '\n';
-
-	//String s2 = "Om";
-	//String s3 = "Thakkar";
-	//std::cout << "Om" + s3 << '\n';
-
-		//String s1{ "Thakkar" };
-		//s1 = std::move(s);
-		////s1 = s;
-		//std::cout << s1 << '\n';
-		//std::cout << s << '\n';
-
-		/*String s1{ "Thakkar" };
-		String s2{ s1 };
-		String s3 = std::move(s1);*/
-
-		/*String s = "Om";
-		String::reverseString(s);
-		std::cout << s;*/
-
-		//std::cin.get();
+	//Date d1{ 31, 12, 2024 };
+	//String s{ "Om" };
+	//try {
+	//	d1.setDate( 32, 12, 2024 );
+	//	auto ch{ s[4] };
+	//}
+	//catch (InvalidDate& e) {
+	//	std::cout << e.getMessage() << '\n';
+	//}
+	//catch (IndexOutOfBound& e) {
+	//	std::cout << e.getMessage() << '\n';
+	//}
 }
